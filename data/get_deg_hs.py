8658c7adae27d02368e4d11236fb5762f3e435ca
Saydemr <abdullah.saydemir@ozu.edu.tr>
2021-12-19 20:27:24 +0300

    Results for num occurences > n for a gene

--- data/get_deg_hs.py
+++ data/get_deg_hs.py
@@ -8,3 +8,22 @@ for i in range(2006,2027):
                 line = line.split('\t')
                 if line[7] == 'Homo sapiens' and line[0] == dataset:
                     g.write(line[0] + '\t' + line[1] + '\t' + line[2] + '\t' + line[3] + '\t' + line[4] + '\t' + line[5] + '\t' + line[6] + '\t' + line[7] + '\t' + line[8] + '\n')
+
+
+gene_num_listed = {}
+
+for i in range(2006,2027):
+    with open ('deg_hs_' + str(i) + '.dat', 'r') as g:
+        for line in g:
+            line = line.strip()
+            line = line.split('\t')
+            if line[2] in gene_num_listed:
+                gene_num_listed[line[2]] += 1
+            else:
+                gene_num_listed[line[2]] = 1
+
+for i in range(1,18):
+    with open ('deg_hs_n-' + str(i) + '.dat', 'w+') as g:
+        for key in gene_num_listed:
+            if gene_num_listed[key] >= i:
+                g.write(key + '\n')
