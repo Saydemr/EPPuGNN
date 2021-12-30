a4e662697d0026a56769c421aac390e890ba2590
Burcu Arslan <burcu.arslan@ozu.edu.tr>
2021-12-30 19:05:05 +0300

    Grand inputs for hs

--- GraphSAGE/networkx_parser.py
+++ GraphSAGE/networkx_parser.py
@@ -5,7 +5,8 @@ from random import choices
 from collections import Counter
 import json
 import numpy as np
-
+import scipy as sp
+from scipy import sparse
 
 print("Loading graph...")
 ppi_graph = nx.Graph() 
@@ -14,6 +15,8 @@ id_map = {}
 id_map_int = {}
 id_map_inv = {}
 id_map_inv_int = {}
+
+np_adj_matrix = np.zeros((27213,27213), dtype=np.int8)
 
 with open("BIOGRID-ORGANISM-Homo_sapiens-4.4.204.tab3.txt") as f:
     f.readline()
@@ -43,6 +46,8 @@ with open("BIOGRID-ORGANISM-Homo_sapiens-4.4.204.tab3.txt") as f:
             id_map_inv_int[i+1] = int(line[2])
             ppi_graph.nodes[i+1]['id'] = i+1
             
+            np_adj_matrix[i][i+1] = 1
+            np_adj_matrix[i+1][i] = 1
             #print(i, i+1)
             i+=2
 
@@ -56,6 +61,9 @@ with open("BIOGRID-ORGANISM-Homo_sapiens-4.4.204.tab3.txt") as f:
             id_map_inv_int[i] = int(line[2])
 
             ppi_graph.nodes[i]['id'] = i
+
+            np_adj_matrix[id_map_int[int(line[1])]][i] = 1
+            np_adj_matrix[i][id_map_int[int(line[1])]] = 1
             #print(id_map_int[int(line[1])], i)
             i+=1
 
@@ -69,13 +77,19 @@ with open("BIOGRID-ORGANISM-Homo_sapiens-4.4.204.tab3.txt") as f:
             id_map_inv[i] = line[1]
             id_map_inv_int[i] = int(line[1])
             ppi_graph.nodes[i]['id'] = i
+
+            np_adj_matrix[i][id_map_int[int(line[2])]] = 1
+            np_adj_matrix[id_map_int[int(line[2])]][i] = 1     
             
             #print(i, id_map_int[int(line[2])])
             i+=1
 
         else:
             ppi_graph.add_edge(id_map_int[int(line[1])],id_map_int[int(line[2])])
+            np_adj_matrix[id_map_int[int(line[1])]][id_map_int[int(line[2])]] = 1
+            np_adj_matrix[id_map_int[int(line[2])]][id_map_int[int(line[1])]] = 1
 
+sp.sparse.save_npz('../grand_blend/adj_matrix_grand.npz', sp.sparse.csr_matrix(np_adj_matrix))
 
 # 0 : training = train_removed false : test_removed true
 # 1 : test     = train_removed true  : test_removed false
@@ -84,7 +98,6 @@ with open("BIOGRID-ORGANISM-Homo_sapiens-4.4.204.tab3.txt") as f:
 
 print("Graph info...")
 print("Number of nodes: ", ppi_graph.number_of_nodes())
-print("Number of connected components", nx.number_connected_components(ppi_graph))
 print("Number of edges: ", ppi_graph.number_of_edges())
 print()
 
@@ -96,7 +109,7 @@ with open("hs_ppi_graph.txt", "w+") as f:
 population = [0, 1, 2]
 weights    = [0.8, 0.1, 0.1]
 distribution_samples = choices(population, weights, k=ppi_graph.number_of_nodes())
-print("Number of instances in training (0), test (1) and validation (2)\n",Counter(distribution_samples),sep='\n')
+print("Number of instances in training (0), test (1) and validation (2)",Counter(distribution_samples),sep='\n')
 print("Number of instances in training (0), test (1) and validation (2)\n",Counter(distribution_samples),sep='\n', file=open("distribution_samples.txt", "w+"))
 for i in range(ppi_graph.number_of_nodes()):
     if distribution_samples[i] == 0:
@@ -112,16 +125,9 @@ for i in range(ppi_graph.number_of_nodes()):
     #print(i)
     #print(ppi_graph.nodes[id_map_inv[i]]['test'], ppi_graph.nodes[id_map_inv[i]]['val'], sep="\t", end="\n")
 
-
-for component in list(nx.connected_components(ppi_graph)):
-    if len(component) < 3:
-        for node in component:
-            ppi_graph.remove_node(node)
-
-
+print()
 print("Checking the graph if smth is modified.")
 print("Number of nodes: ", ppi_graph.number_of_nodes())
-print("Number of connected components", nx.number_connected_components(ppi_graph))
 print("Number of edges: ", ppi_graph.number_of_edges())
 
 id_name_dict ={}
@@ -134,32 +140,41 @@ with open('BIOGRID-ORGANISM-Homo_sapiens-4.4.204.tab3.txt') as f:
         id_name_dict[line[1]] = line[7]
         id_name_dict[line[2]] = line[8]
 
+
 essential_dict = set()
 with open('deg_hs_n-4.dat') as f:
     for line in f:
         essential_dict.add(line.strip())
 
+
+y_mat = np.zeros((ppi_graph.number_of_nodes(),1), dtype=np.int8)
 class_map = {}
 for i in id_map:
     my_key = id_map[i]
     my_str = id_name_dict[i]
     if my_str in essential_dict:
         class_map[my_key] = 1
+        y_mat[my_key] = 1
     else:
         class_map[my_key] = 0
+
+np.save('../grand_blend/y_mat_grand.npy', y_mat)
 
 print("Number of essential genes", sum(class_map.values()))
 print()
 
 print("Creating class-map")
 print('Creating id-map')
+
 id_mappppp = {}
 for i in range(ppi_graph.number_of_nodes()):
     id_mappppp[str(i)] = i
+
 
 print("Writing graph to JSON file...")
 json.dump(class_map, fp=open("hs_eppugnn-class_map.json", "w+"))
 json.dump(json_graph.node_link_data(ppi_graph), fp=open("hs_eppugnn-G.json", "w+"))
 json.dump({str(v): int(k) for k, v in id_map.items()}, fp=open("hs_eppugnn-id_map_inv.json", "w+"))
+json.dump({str(v): int(k) for k, v in id_map.items()}, fp=open("../data/hs_eppugnn-id_map_inv.json", "w+"))
 json.dump(id_mappppp, fp=open("hs_eppugnn-id_map.json", "w+"))
 json.dump(id_map, fp=open("hs_eppugnn-id_map_dummy.json", "w+"))
